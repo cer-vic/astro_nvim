34cf68df7232b512270b4148b4a8eef7c6936506
Victor Cernețchi <victor.cernetchi@amdaris.com>
2023-05-23 12:36:19 +0300

    update plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -29,6 +29,8 @@ return {
         },
       },
     },
+
+  {import = "astrocommunity.color.twilight-nvim"},
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
