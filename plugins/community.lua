974cc72aa251e2f85f0f6c7fc3fd372a68e55663
Victor Cernețchi <victor.cernetchi@amdaris.com>
2023-05-23 12:07:37 +0300

    add copilot config

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,6 +1,34 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.completion.copilot-lua" },
+  {
+      "copilot.lua",
+      opts = {
+        panel = {
+          auto_refresh = true,
+          keymap = {
+            accept = "<C-l>",
+            jump_next = "<C-j>",
+            jump_prev = "<C-k>",
+            dismiss = "<C-h>",
+          },
+          layout = {
+            position = "right",
+          },
+        },
+        suggestion = {
+          keymap = {
+            accept = "<C-l>",
+            accept_word = false,
+            accept_line = false,
+            next = "<C-j>",
+            prev = "<C-k>",
+            dismiss = "<C-h>",
+          },
+        },
+      },
+    },
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
