b05b34400a35b3f0ef62c6cd98419392584cbdbe
Victor Cernețchi <victor.cernetchi@amdaris.com>
2023-07-21 16:09:51 +0300

    remove copilot

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,34 +1,34 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.completion.copilot-lua" },
-  {
-      "copilot.lua",
-      opts = {
-        panel = {
-          auto_refresh = true,
-          keymap = {
-            accept = "<C-l>",
-            jump_next = "<C-j>",
-            jump_prev = "<C-k>",
-            dismiss = "<C-h>",
-          },
-          layout = {
-            position = "right",
-          },
-        },
-        suggestion = {
-          keymap = {
-            accept = "<C-l>",
-            accept_word = false,
-            accept_line = false,
-            next = "<C-j>",
-            prev = "<C-k>",
-            dismiss = "<C-h>",
-          },
-        },
-      },
-    },
+  -- { import = "astrocommunity.completion.copilot-lua" },
+  -- {
+  --     "copilot.lua",
+  --     opts = {
+  --       panel = {
+  --         auto_refresh = true,
+  --         keymap = {
+  --           accept = "<C-l>",
+  --           jump_next = "<C-j>",
+  --           jump_prev = "<C-k>",
+  --           dismiss = "<C-h>",
+  --         },
+  --         layout = {
+  --           position = "right",
+  --         },
+  --       },
+  --       suggestion = {
+  --         keymap = {
+  --           accept = "<C-l>",
+  --           accept_word = false,
+  --           accept_line = false,
+  --           next = "<C-j>",
+  --           prev = "<C-k>",
+  --           dismiss = "<C-h>",
+  --         },
+  --       },
+  --     },
+  --   },
   {import = "astrocommunity.color.twilight-nvim"},
   {import = "astrocommunity.editing-support.todo-comments-nvim"},
   {import = "astrocommunity.editing-support.neogen"},
